bfc73f26dc399bc96c28b41b3d083895311ace59
Mohammedfarish786 <mdfarish72@gmail.com>
2025-05-28 17:18:02 +0530

    routing of requisition page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import LoginWeb from './components/loginpage/loginpage';
-import Layout from './components/layout/MainLayout';
-import Dashboard from './components/dashboard/dashboard';
-import AddExpense from './components/expenses/add_expense/add_expense';
-import MyExpense from './components/expenses/my_expense/my_expense';
-import AllExpenses from './components/expenses/all_expenses/all_expenses';
-import RequestedExpenses from './components/expenses/requested_expenses/requested_expenses';
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import LoginWeb from "./components/loginpage/loginpage";
+import Layout from "./components/layout/MainLayout";
+import Dashboard from "./components/dashboard/dashboard";
+import AddExpense from "./components/expenses/add_expense/add_expense";
+import MyExpense from "./components/expenses/my_expense/my_expense";
+import AllExpenses from "./components/expenses/all_expenses/all_expenses";
+import RequestedExpenses from "./components/expenses/requested_expenses/requested_expenses";
+import RequisitionFormWeb from "./components/requisitionpage/addrequisition";
+import AllRequisitions from "./components/requisitionpage/allrequisition";
+import MyRequisitions from "./components/requisitionpage/myrequisition";
+import RequisitionsWeb from "./components/requisitionpage/requested_requisition";
 
 function App() {
   return (
@@ -17,30 +19,39 @@ function App() {
         <Route path="/" element={<LoginWeb />} />
         <Route element={<Layout />}>
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/attendance/my" element={<div>My Attendance Page</div>} />
-          <Route path="/attendance/user" element={<div>User Attendance Page</div>} />
+          <Route
+            path="/attendance/my"
+            element={<div>My Attendance Page</div>}
+          />
+          <Route
+            path="/attendance/user"
+            element={<div>User Attendance Page</div>}
+          />
           <Route path="/leaves/add" element={<div>Add Leave Page</div>} />
           <Route path="/leaves/my" element={<div>My Leaves Page</div>} />
           <Route path="/leaves/all" element={<div>All Leaves Page</div>} />
-          <Route path="/leaves/manage" element={<div>Manage Leaves Page</div>} />
+          <Route
+            path="/leaves/manage"
+            element={<div>Manage Leaves Page</div>}
+          />
           <Route path="/expenses/addexpense" element={<AddExpense />} />
           <Route path="/expenses/myexpense" element={<MyExpense />} />
           <Route path="/expenses/allexpenses" element={<AllExpenses />} />
-          <Route path="/expenses/requestedexpenses" element={<RequestedExpenses />} />
-          <Route path="/project/manage" element={<div>Manage Project Page</div>} />
-          <Route path="/requisition/add" element={<div>Add Requisition Page</div>} />
-          <Route path="/requisition/my" element={<div>My Requisitions Page</div>} />
-          <Route path="/requisition/all" element={<div>All Requisitions Page</div>} />
-          <Route path="/requisition/manage" element={<div>Manage Requisitions Page</div>} />
+          <Route
+            path="/expenses/requestedexpenses"
+            element={<RequestedExpenses />}
+          />
+          <Route
+            path="/project/manage"
+            element={<div>Manage Project Page</div>}
+          />
+          <Route path="/requisition/add" element={<RequisitionFormWeb />} />
+          <Route path="/requisition/my" element={<MyRequisitions />} />
+          <Route path="/requisition/all" element={<AllRequisitions />} />
+          <Route path="/requisition/manage" element={<RequisitionsWeb />} />
         </Route>
       </Routes>
     </BrowserRouter>
-
-
-
-
-
-
   );
 }
 
