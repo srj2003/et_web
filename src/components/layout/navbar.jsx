494df8eb84c20c444cf8c553521c404dba6e1a81
Srinjoy Saha <srinjoysxi69@gmail.com>
2025-06-06 12:01:19 +0530

    notification button removed

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -128,43 +128,7 @@ const Navbar = () => {
       </div>
 
       <div className="navbar-right">
-        <div className="notification-container">
-          {/* <button
-            className="notification-button"
-            onClick={() => setShowNotifications(!showNotifications)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-              <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-            </svg>
-            {notifications.length > 0 && (
-              <span className="notification-badge">{notifications.length}</span>
-            )}
-          </button> */}
-
-          {showNotifications && (
-            <div className="notification-dropdown">
-              {notifications.map((notification) => (
-                <div key={notification.id} className="notification-item">
-                  <div className="notification-content">
-                    {notification.message}
-                  </div>
-                  <div className="notification-time">{notification.time}</div>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+        
 
         <div className="profile-container">
           <button
